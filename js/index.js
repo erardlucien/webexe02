4d3bcb8f8692d541e594e011912c86148d3e3f3d
erardlucien <erardlaurent1@gmail.com>
2024-04-11 10:44:38 +0200

    The animation has changed.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 'use strict';
 
 let unanimatedObject = document.querySelector('.unanimated_object');
-unanimatedObject.classList.add('animated_object');
 
-(
-    function animating() {
-        unanimatedObject.classList.toggle('animated_object');
-        setTimeout(animating, 3100);
+let intermediate = [null, null];
+let end = null;
+
+function animation() {
+
+    intermediate[0] = setTimeout(() => {
+        unanimatedObject.classList.add('between_state');
+    }, 100);
+
+    intermediate[1] = setTimeout(() => {
+        unanimatedObject.classList.remove('between_state');
+        unanimatedObject.classList.add('animated_object');
+    }, 500);
+
+    end = setTimeout( () => {
+        unanimatedObject.classList.remove('animated_object');
+        clearTimeout(intermediate[0]);
+        clearTimeout(intermediate[1]);
     }
-)();
+    , 900);
+}
+
+let repeatAnimation = setInterval(animation, 1500);
